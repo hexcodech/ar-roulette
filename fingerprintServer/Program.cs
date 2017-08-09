86a00fade6de6edf9b00fb6bb014b9f77c9f3936
Marc Berchtold <marc.berchtold@hotmail.de>
2017-08-09 21:43:55 +0200

    Basic fingerprint detection in fingerprintServer

--- fingerprintServer/Program.cs
+++ fingerprintServer/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Drawing;
+using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
 using libzkfpcsharp;
 
 namespace fingerprintServer
@@ -14,6 +12,7 @@ namespace fingerprintServer
     IntPtr FormHandle = IntPtr.Zero;
     bool bIsTimeToDie = false;
     bool IsRegister = false;
+    bool bIdentify = true;
     byte[] FPBuffer;
     int RegisterCount = 0;
     const int REGISTER_FINGER_COUNT = 3;
@@ -33,8 +32,7 @@ namespace fingerprintServer
 
       program.InitializeDevice();
       program.OpenDevice();
-
-      Console.ReadLine();
+      program.StartMenuLoop();
     }
 
     private void InitializeDevice()
@@ -88,9 +86,162 @@ namespace fingerprintServer
         if (ret == zkfp.ZKFP_ERR_OK)
         {
           Console.WriteLine("Finger captured: OK");
+          Handler("MESSAGE_CAPTURED_OK");
         }
         Thread.Sleep(200);
       }
+    }
+
+    private void Handler(string message)
+    {
+      switch (message)
+      {
+        case "MESSAGE_CAPTURED_OK":
+          {
+            MemoryStream ms = new MemoryStream();
+            BitmapFormat.GetBitmap(FPBuffer, fpInstance.imageWidth, fpInstance.imageHeight, ref ms);
+            Bitmap bmp = new Bitmap(ms);
+            // this.picFPImg.Image = bmp;
+
+            // We scanned a finger and we're trying to register a new finger.
+            if (IsRegister)
+            {
+              int ret;
+              int fid = 0;
+              int score = 0;
+
+              ret = fpInstance.Identify(CapTmp, ref fid, ref score);
+              if (ret == zkfp.ZKFP_ERR_OK)
+              {
+                Console.WriteLine("This finger is already registered by " + fid);
+                return;
+              }
+              if (RegisterCount > 0 && fpInstance.Match(CapTmp, RegTmps[RegisterCount - 1]) <= 0)
+              {
+                Console.WriteLine("Press finger 3 times to register");
+                return;
+              }
+
+              Array.Copy(CapTmp, RegTmps[RegisterCount], cbCapTmp);
+              RegisterCount++;
+
+              // If the new finger has been scanned enough times
+              if (RegisterCount >= REGISTER_FINGER_COUNT)
+              {
+                RegisterCount = 0;
+                // If we successfully generate a template from the finger and save it
+                if (fpInstance.GenerateRegTemplate(RegTmps[0], RegTmps[1], RegTmps[2], RegTmp, ref cbRegTmp) ==
+                    zkfp.ZKFP_ERR_OK && fpInstance.AddRegTemplate(iFid, RegTmp) == zkfp.ZKFP_ERR_OK)
+                {
+                  iFid++;
+                  Console.WriteLine("Saved finger successfully");
+                }
+                else
+                {
+                  Console.WriteLine("Failed to register finger code=" + ret);
+                }
+
+                // End register
+                IsRegister = false;
+                return;
+              }
+              else
+              {
+                Console.WriteLine("Press the finger " + (REGISTER_FINGER_COUNT - RegisterCount) + " times to register");
+              }
+            }
+            // Check finger
+            else
+            {
+              if (cbRegTmp <= 0)
+              {
+                Console.WriteLine("No fingers registered yet, register first");
+                return;
+              }
+              if (bIdentify)
+              {
+                int fid = 0;
+                int score = 0;
+                int ret = fpInstance.Identify(CapTmp, ref fid, ref score);
+
+                if (ret == zkfp.ZKFP_ERR_OK)
+                {
+                  Console.WriteLine("Successfully identified finger fid=" + fid + ", score=" + score);
+                  return;
+                }
+                else
+                {
+                  Console.WriteLine("Identifying failed");
+                  return;
+                }
+              }
+              else
+              {
+                int ret = fpInstance.Match(CapTmp, RegTmp);
+                if (ret > 0)
+                {
+                  Console.WriteLine("Matching finger successfully score=" + ret);
+                  return;
+                }
+                else
+                {
+                  Console.WriteLine("Matching finger failed");
+                  return;
+                }
+              }
+            }
+          }
+          break;
+
+        default:
+          Handler(message);
+          break;
+      }
+    }
+
+    private void StartMenuLoop()
+    {
+      Console.WriteLine("------------------------------------");
+      Console.WriteLine("Press ESC to stop");
+      Console.WriteLine("Press r key to register a new finger");
+      Console.WriteLine("Press i key to identify your finger");
+      Console.WriteLine("Press v key to verify your finger");
+      Console.WriteLine("------------------------------------");
+      do
+      {
+        switch (Console.ReadKey(true).Key)
+        {
+          case ConsoleKey.R:
+          {
+            if (!IsRegister)
+            {
+              IsRegister = true;
+              RegisterCount = 0;
+              cbRegTmp = 0;
+              Console.WriteLine("Registering your finger... Press finger 3 times");
+            }
+          }
+            break;
+          case ConsoleKey.I:
+          {
+            if (!bIdentify)
+            {
+              bIdentify = true;
+              Console.WriteLine("Identifying your finger... Press finger once");
+            }
+          }
+            break;
+          case ConsoleKey.V:
+          {
+            if (bIdentify)
+            {
+              bIdentify = false;
+              Console.WriteLine("Verifying your finger... Press finger once");
+            }
+          }
+            break;
+        }
+      } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
     }
   }
 }
